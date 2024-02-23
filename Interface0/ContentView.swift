d0979b692cd05d437dc5515f5452a410335c40c8
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-02-23 21:20:07 +0530

    Initial Commit

---
+++ Interface0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Interface0
+//
+//  Created by Rafat on 23/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Interface0/Interface0App.swift
@@ -0,0 +1,17 @@
+//
+//  Interface0App.swift
+//  Interface0
+//
+//  Created by Rafat on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Interface0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
