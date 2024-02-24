37c1808f835d8097251435e2db639a81be5693ff
Rafat Siddiqui <mdrafat.siddiqui@outlook.com>
2024-02-24 09:22:47 +0530

    Dropping this and cloning official Grape Library

---
+++ Interface0/FDG7.swift
@@ -0,0 +1,18 @@
+//
+//  FDG7.swift
+//  Interface0
+//
+//  Created by Rafat on 24/02/24.
+//
+
+import SwiftUI
+
+struct FDG7: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FDG7()
+}
